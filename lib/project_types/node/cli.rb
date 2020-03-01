122345c80664cef1da00e893e689727ff1f65a55
Tim Anema <tim.anema@shopify.com>
2020-03-01 15:36:38 -0500

    Add some extra flair

--- lib/project_types/node/cli.rb
+++ lib/project_types/node/cli.rb
@@ -1,9 +1,15 @@
 module Node
   class Project < ShopifyCli::ProjectType
     register_command('Node::Commands::Serve', "serve")
+    # register_task('Node::Tasks::NodeTask', 'node_task')
   end
 
+  # define/autoload project specific Commads
   module Commands
     autoload :Serve, 'project_types/node/commands/serve'
+  end
+
+  # define/autoload project specific Tasks
+  module Tasks
   end
 end
--- lib/project_types/rails/cli.rb
+++ lib/project_types/rails/cli.rb
@@ -1,9 +1,15 @@
 module Rails
   class Project < ShopifyCli::ProjectType
     register_command('Rails::Commands::Serve', "serve")
+    # register_task('Rails::Tasks::RailsTask', 'rails_task')
   end
 
+  # define/autoload project specific Commads
   module Commands
     autoload :Serve, 'project_types/rails/commands/serve'
+  end
+
+  # define/autoload project specific Tasks
+  module Tasks
   end
 end
